ffb9af5d1e7f924f3f35291a9eeceecfde8021c5
Your Name <you@example.com>
2022-05-03 22:00:08 -0400

    Se limpia el main

--- main.c
+++ main.c
@@ -4,31 +4,8 @@
 #include <string.h>
 #include "util.h"
 
-void showList (List* list)
-{
-    char* id = listFirst(list);
-
-    while (id != NULL)
-    {
-        printf("%s\n", id);
-        id = listNext(list);
-    }     
-}
-
-List * readBooks() {
-    printf("Ingrese los ID de los libros a leer: ");
-    char ids[100];
-
-    fflush(stdin);
-    scanf("%[^\n]*s", ids);
-    getchar();
-
-    List *ret = strToList(ids, " ");
-
-    return ret;
-}
-
 void lower_than(void *k1, void *k2);
+
 int main() {
     List *books = readBooks();
     showList (books);

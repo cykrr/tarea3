5df9fe8174ffc3ce5cf18e4640d4432168d1f9eb
Benjamín Robles <benjamin.robles.a@mail.pucv.cl>
2022-05-03 23:53:34 -0400

    se añade createBook

--- main.c
+++ main.c
@@ -5,6 +5,36 @@
 #include "util.h"
 #include "book.h"
 
+Book *createBook(char *id) {
+    Book *book = malloc(sizeof(Book));
+    if(!book) {
+        printf("Error guardando memoria para Book\n");
+    }
+
+    strcpy(book->id, id);
+
+    char fileName[100];
+    strcpy(fileName, "books/");
+    strcat(fileName, id);
+    strcat(fileName, ".txt");
+
+    book->fd = fopen(fileName, "r");
+
+    if(!book->fd) {
+        printf("El archivo %s no existe\n", fileName);
+        free(book);
+        return NULL; 
+    }
+
+    for(int i = 0; i < 34; i++) fgetc(book->fd);
+    fgets(book->title, 100*sizeof(char), book->fd);
+
+    char *comma = strchr(book->title, ',');
+    if(comma != NULL) *comma = '\0';
+
+    return book;
+}
+
 void loadBooks(List* books, TreeMap* sortedBooks)
 {
     char* id = listFirst(books);

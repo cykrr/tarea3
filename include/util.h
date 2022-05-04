bd65f2e1215e98a7261f08a6c52b2594fd43f27d
Your Name <you@example.com>
2022-05-03 21:53:51 -0400

    Se arregla la funcion strToList

--- include/util.h
+++ include/util.h
@@ -2,10 +2,11 @@
 #define UTIL_H
 #include "list.h"
 
+#ifndef WIN32
 char *_strdup(const char *str);
+#endif
 
 //Crea la lista de generos y guarda los mismos al detectarlos
-List *strToList(const char *string, const char delim);
-
+List *strToList(const char *string, const char* delim);
 
 #endif
--- src/util.c
+++ src/util.c
@@ -1,27 +1,26 @@
 #include "util.h"
 #include <string.h>
 
-char * _strdup(const char * str) {
-    char * aux = (char *)malloc(strlen(str) + 1);
-    strcpy(aux, str);
-    return aux;
-}
+#ifndef WIN32
+    char * _strdup(const char * str) {
+        char * aux = (char *)malloc(strlen(str) + 1);
+        strcpy(aux, str);
+        return aux;
+    }
+#endif
+
 
 //Crea la lista de generos y guarda los mismos al detectarlos
-List *strToList(const char *string, const char delim){
+List *strToList(const char *string, const char* delim){
     List *list = listCreate();
 
     char *aux = _strdup(string);
-    char *token = strtok(aux, &delim);
-
-    int flag = 0;
-    while(token != NULL){
-        if (flag) listPushBack(list, token+1);
-        else listPushBack(list, token);
-        flag = 1;
-
-        token = strtok(NULL, &delim);
-    }
-        
+    char *token = strtok(aux, delim);
+    while (token != NULL)
+    {
+        listPushBack(list, token);
+        token = strtok(NULL, delim);
+    }  
+            
     return list;
 }

a276f40b165e6eaa8ac3d9936a3ccb5ed893653f
Your Name <you@example.com>
2022-05-16 09:03:14 -0400

    eliminar funciones del main

--- main.c
+++ main.c
@@ -7,66 +7,6 @@
 #include "book.h"
 #include "menu.h"
 
-Book *createBook(char *id) {
-    Book *book = malloc(sizeof(Book));
-    if(!book) {
-        printf("Error guardando memoria para Book\n");
-    }
-
-    strcpy(book->id, id);
-
-    char fileName[100];
-    strcpy(fileName, "books/");
-    strcat(fileName, id);
-    strcat(fileName, ".txt");
-
-    book->fd = fopen(fileName, "r");
-
-    if(!book->fd) {
-        printf("El archivo %s no existe\n", fileName);
-        free(book);
-        return NULL; 
-    }
-
-    // Avanzar 34 caracteres para leer el titulo.
-    for(int i = 0; i < 34; i++) fgetc(book->fd);
-    fgets(book->title, 100*sizeof(char), book->fd);
-
-    // Eliminar el Autor si es que existe.
-    char *comma = strchr(book->title, ',');
-    if(comma != NULL) *comma = '\0';
-
-    return book;
-}
-
-void showBooks(TreeMap *sortedMap) 
-{
-    Pair *bookPair = firstTreeMap(sortedMap);
-    Book *book;
-    while (bookPair != NULL) 
-    {
-        book = bookPair->value;
-
-        printf("ID: %s\n", book->id);
-        printf("Title: %s\n", book->title);
-
-        bookPair = nextTreeMap(sortedMap);
-    }
-}
-
-void loadBooks(List* books, TreeMap* sortedBooks)
-{
-    char* id = listFirst(books);
-    while (id != NULL)
-    {
-        Book *book = createBook(id);
-
-        if(book != NULL) 
-            insertTreeMap(sortedBooks, book->title, book);
-
-        id = listNext(books);
-    }
-}
 
 int main() {
     List *books = NULL; 

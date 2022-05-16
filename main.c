6700b822894e79d76f994193e8bb92cbfeaf2ba9
Your Name <you@example.com>
2022-05-16 08:52:41 -0400

    añadir menu

--- main.c
+++ main.c
@@ -2,8 +2,10 @@
 #include "treemap.h"
 #include "list.h"
 #include <string.h>
+
 #include "util.h"
 #include "book.h"
+#include "menu.h"
 
 Book *createBook(char *id) {
     Book *book = malloc(sizeof(Book));
@@ -67,13 +69,31 @@ void loadBooks(List* books, TreeMap* sortedBooks)
 }
 
 int main() {
-    List *books = readBooks();
+    List *books = NULL; 
     TreeMap* sortedBooks = createTreeMap(lower_than_string);
     //TreeMap* wordFrecuency = createTreeMap(lower_than_string);
     //showList (books);
-    loadBooks(books, sortedBooks);
 
-    showBooks(sortedBooks);
+
+    char in = 0;
+    while ( in != 'q') {
+        showMenu();
+        scanf("%c", &in);
+        getchar(); // Eliminar \n del stdin
+
+        switch (in) {
+            case 'i':
+                books = readBooks();
+                loadBooks(books, sortedBooks);
+                break;
+            case 'm':
+                showBooks(sortedBooks);
+                break;
+            case 'b':
+//                searchBooks(sortedBooks);
+                break;
+        }
+    }
 
     return 0;
 }
---
+++ src/menu.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "menu.h"
+void showMenu() {
+    printf("Biblioteca\n");
+    printf("i: Importar archivos\n");
+    printf("m: Mostrar libros\n");
+    printf("b: Buscar libros por palabra\n");
+    printf("q: Salir\n");
+}

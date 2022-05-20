929b18591fe5f8e33fd22ee452c0c3fa359e7aa7
Arturo Reyes <arturo.reyes.c01@mail.pucv.cl>
2022-05-20 15:34:48 -0400

    Se añaden las palabras que mas se repetian

--- include/exclude.h
+++ include/exclude.h
@@ -1,11 +1,16 @@
 #ifndef EXCLUDE_H
 #define EXCLUDE_H
-char excludeWords[][20] = {
-    "el",
-    "ella",
-    "la", 
-    "los",
-    "nosotros",
-};
+char excludeWords[][100] = {"the", "and", "of", "to", "that", 
+    "in", "he", "shall", "unto", "for", "a", "was", "it",
+    "she", "said", "you", "be", "an", "have", "i", "not",
+    "on", "with", "as", "do", "at", "this", "but", "his",
+    "by", "from", "they", "we", "say", "her", "or", "will"
+    "my", "one", "all", "would", "there", "their", "what", 
+    "so", "up", "out", "if", "about", "who", "get", "which",
+    "go", "me", "when", "make", "can", "like", "no", "just",
+    "him", "know", "take", "into", "your", "good", "some", 
+    "could", "them", "than", "then", "now", "come", "its",
+    "also", "how", "our", "well", "even", "want", "because",
+    "any", "most", "us", "are", "is"};
 #endif
 

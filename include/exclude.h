3e108b26514a2b2c4c835d1bfa7470e34c728baf
Arturo Reyes <arturo.reyes.c01@mail.pucv.cl>
2022-05-20 16:48:35 -0400

    Se añade la lista de exlusion

--- include/exclude.h
+++ include/exclude.h
@@ -1,16 +1,17 @@
 #ifndef EXCLUDE_H
 #define EXCLUDE_H
-char excludeWords[][100] = {"the", "and", "of", "to", "that", 
-    "in", "he", "shall", "unto", "for", "a", "was", "it",
-    "she", "said", "you", "be", "an", "have", "i", "not",
-    "on", "with", "as", "do", "at", "this", "but", "his",
-    "by", "from", "they", "we", "say", "her", "or", "will"
-    "my", "one", "all", "would", "there", "their", "what", 
-    "so", "up", "out", "if", "about", "who", "get", "which",
-    "go", "me", "when", "make", "can", "like", "no", "just",
-    "him", "know", "take", "into", "your", "good", "some", 
-    "could", "them", "than", "then", "now", "come", "its",
-    "also", "how", "our", "well", "even", "want", "because",
-    "any", "most", "us", "are", "is"};
+
+    char excludeWords[][100] = {"the", "and", "of", "to", "that", 
+        "in", "he", "shall", "unto", "for", "a", "was", "it",
+        "she", "said", "you", "be", "an", "have", "i", "not",
+        "on", "with", "as", "do", "at", "this", "but", "his",
+        "by", "from", "they", "we", "say", "her", "or", "will"
+        "my", "one", "all", "would", "there", "their", "what", 
+        "so", "up", "out", "if", "about", "who", "get", "which",
+        "go", "me", "when", "make", "can", "like", "no", "just",
+        "him", "know", "take", "into", "your", "good", "some", 
+        "could", "them", "than", "then", "now", "come", "its",
+        "also", "how", "our", "well", "even", "want", "because",
+        "any", "most", "us", "are", "is"};
 #endif
 

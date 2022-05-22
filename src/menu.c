d74970381242f296cc405260a5cc64e8ff701f3e
Benjamín Robles <benjamin.robles.a@mail.pucv.cl>
2022-05-21 22:18:39 -0400

    cambiar titulo funcion

--- src/menu.c
+++ src/menu.c
@@ -4,7 +4,7 @@ void showMenu() {
     printf("Biblioteca\n");
     printf("1: Importar archivos\n");
     printf("2: Mostrar libros\n");
-    printf("3: Buscar libro por titulo\n");
+    printf("3: Buscar libro por coincidencias\n");
     printf("4: Palabras con mayor frecuencia\n");
     printf("5: Palabras más relevantes\n");
     printf("6: Buscar libros por palabra\n");
